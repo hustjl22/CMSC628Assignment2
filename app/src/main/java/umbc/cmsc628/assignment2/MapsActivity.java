ce07074816995f1c4ef1de0bce9d24071a7a674b
Taylor Nycum <taylor.nycum@gmail.com>
2016-04-19 18:51:00 -0400

    Removed database functionality, added last known location and non functional search

--- app/src/main/java/umbc/cmsc628/assignment2/MapsActivity.java
+++ app/src/main/java/umbc/cmsc628/assignment2/MapsActivity.java
@@ -1,37 +1,42 @@
 package umbc.cmsc628.assignment2;
 
 import android.Manifest;
-import android.content.ContentValues;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.Date;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final String TAG = MapsActivity.class.getName();
     private static final int MY_PERMISSIONS_GET_FINE_LOCATION = 0;
+    private static final int ZOOM_LEVEL = 15;
 
     private GoogleMap mMap;
     private LocationManager locationManager;
-    private UserLocationDbHelper dbHelper;
-    private SQLiteDatabase db;
+    private GoogleApiClient googleApiClient;
+    private Location lastKnownLcation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,8 +47,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        dbHelper = new UserLocationDbHelper(this);
-        db = dbHelper.getWritableDatabase();
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .addApi(Places.GEO_DATA_API)
+                    .addApi(Places.PLACE_DETECTION_API)
+                    .build();
+        }
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment) getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.e(TAG, "An error occurred: " + status);
+            }
+        });
     }
 
     @Override
@@ -61,8 +86,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch(requestCode) {
+        switch (requestCode) {
             case MY_PERMISSIONS_GET_FINE_LOCATION: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
@@ -97,16 +134,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng sydney = new LatLng(40, 40);
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Last known location may not be known yet so just move to approximately where the user is
+        // likely to be in the meantime
+        LatLng maryland = new LatLng(40, 40);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(maryland));
     }
 
     @Override
     public void onLocationChanged(Location location) {
+        mMap.clear();
         Log.d(TAG, "Location changed: lat=" +
                 Double.toString(location.getLatitude()) +
                 " long=" + Double.toString(location.getLatitude()));
-        new UpdateLocationTask().execute(location);
+
+        LatLng newLocation = new LatLng(location.getLatitude(), location.getLongitude());
+
+        setMarkerOnLocation(newLocation);
+
+        // Move the camera to the new location
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(newLocation, ZOOM_LEVEL);
+        mMap.animateCamera(cameraUpdate);
     }
 
     @Override
@@ -124,65 +171,48 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    private class UpdateLocationTask extends AsyncTask<Location, Integer, Long> {
-
-        @Override
-        protected Long doInBackground(Location... locs) {
-            long lastRowId = -1;
-
-            for (Location loc : locs) {
-                lastRowId = writeLocationToDatabase(loc);
-            }
-
-            return lastRowId;
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "On connected called.");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        lastKnownLcation = LocationServices.FusedLocationApi.getLastLocation(
+                googleApiClient);
+        if (lastKnownLcation != null) {
+            double latitude = lastKnownLcation.getLatitude();
+            double longitude = lastKnownLcation.getLongitude();
+            LatLng lastLatLng = new LatLng(latitude, longitude);
 
-        @Override
-        protected void onPostExecute(Long lastRowInserted) {
-            super.onPostExecute(lastRowInserted);
+            setMarkerOnLocation(lastLatLng);
 
-            LatLng lastLatLng = getLastLatLng();
-            if (lastLatLng != null) {
-                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(lastLatLng, 10);
-                mMap.animateCamera(cameraUpdate);
-            } else {
-                Log.e(TAG, "No last lat/long found");
-            }
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(lastLatLng, ZOOM_LEVEL);
+            mMap.animateCamera(cameraUpdate);
         }
+    }
 
-        private long writeLocationToDatabase(Location loc) {
-            ContentValues values = createLocationContentValues(loc);
+    @Override
+    public void onConnectionSuspended(int i) {
 
-            long newRowId;
-            newRowId = db.insert(UserLocationContract.UserLocationEntry.TABLE_NAME,
-                    null,
-                    values);
+    }
 
-            return newRowId;
-        }
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 
-        private ContentValues createLocationContentValues(Location loc) {
-            ContentValues values = new ContentValues();
+    }
 
-            String timestamp = new Date().toString();
-            String latitudeString = Double.toString(loc.getLatitude());
-            String longitudeString = Double.toString(loc.getLongitude());
-
-            values.put(UserLocationContract.UserLocationEntry.COLUMN_NAME_TIMESTAMP, timestamp);
-            values.put(UserLocationContract.UserLocationEntry.COLUMN_NAME_LATITUDE, latitudeString);
-            values.put(UserLocationContract.UserLocationEntry.COLUMN_NAME_LONGITUDE, longitudeString);
-            return values;
-        }
-
-        private LatLng getLastLatLng() {
-            Cursor results = dbHelper.getLastLocation(db);
-            if (results.moveToFirst()) {
-                double latitude = results.getDouble(1);
-                double longitude = results.getDouble(2);
-                results.close();
-                return new LatLng(latitude, longitude);
-            }
-            return null;
-        }
+    private void setMarkerOnLocation(LatLng loc) {
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(loc);
+        markerOptions.draggable(true);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+        mMap.addMarker(markerOptions);
     }
 }
